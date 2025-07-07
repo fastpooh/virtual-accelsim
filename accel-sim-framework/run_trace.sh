fb5127147dd5822f756f12815effa29e16135e35
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 <cwchris@naver.com>
2025-07-07 05:22:32 +0000

    restart

--- accel-sim-framework/run_trace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-./util/tracer_nvbit/run_hw_trace.py -B mixtral_step1 -D 0
-#./util/tracer_nvbit/run_hw_trace.py -B mixtral_step2_GQA -D 0
-#./util/tracer_nvbit/run_hw_trace.py -B mixtral_step2_MHA -D 0
-#./util/tracer_nvbit/run_hw_trace.py -B mixtral_step3 -D 0
-#./util/tracer_nvbit/run_hw_trace.py -B mixtral_step4_FFN -D 0
-#./util/tracer_nvbit/run_hw_trace.py -B mixtral_step5 -D 0
